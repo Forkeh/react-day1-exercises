8202ce8ff5181ccbad7008da57559314bfd475aa
Forkeh <forkeh@gmail.com>
2024-02-26 11:40:02 +0100

    initial commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+interface Props {
+    name: string;
+    email: string;
+    isActive: boolean;
+    singleLine?: boolean;
+}
+
+export default function Profile({ name, email, isActive, singleLine }: Props) {
+    return (
+        <>
+            {!singleLine ? (
+                <>
+                    <h3>Name: {name}</h3>
+                    <p>email: {email}</p>
+                    <p>Active: {isActive ? "Yes" : "No"}</p>
+                </>
+            ) : (
+                <div>
+                    Name: {name} - E-mail: {email} - Active: {isActive ? "Yes" : "No"}
+                </div>
+            )}
+        </>
+    );
+}
---
+++ src/exercises/PropsDemo.tsx
@@ -0,0 +1,18 @@
+import Profile from "../components/Profile";
+import { BaseProps } from "../types";
+import { useState } from "react";
+
+export default function PropsDemo({ title }: BaseProps) {
+    const [showHorizontal, setShowHorizontal] = useState(false);
+    return (
+        <>
+            <h2>{title}</h2>
+            Direction:{" "}
+            <input type="checkbox" checked={showHorizontal} onChange={() => setShowHorizontal(!showHorizontal)} />
+            <Profile name="Max Power" email="mp@email.com" isActive={true} singleLine={showHorizontal} />
+            <Profile name="Martin" email="martin@email.com" isActive={false} singleLine={showHorizontal} />
+            <Profile name="Søren" email="søren@email.com" isActive={true} singleLine={showHorizontal} />
+            {/** Add two or more Profiles, but not until you actually have implemented <Profile />   */}
+        </>
+    );
+}
